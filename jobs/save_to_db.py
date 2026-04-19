d007b865bf19eae6cb1e28d9249bd72a48843ef6
Ibrahim <ibrahim.elaidouni@uit.ac.ma>
2026-04-19 10:53:44 +0100

    fix: use save_only_new_rows for both weather and traffic tables

--- jobs/save_to_db.py
+++ jobs/save_to_db.py
@@ -1,10 +1,8 @@
-# TO DO: PEP8
-from utils.connect import create_spark_session, get_logger
-from utils.data_io import save_in_db , read_parquet_from_minio, save_only_new_rows
 from pyspark.sql.utils import AnalysisException # type: ignore
 from dotenv import load_dotenv #type:ignore
-import os
 
+from utils.data_io import read_parquet_from_minio, save_only_new_rows
+from utils.connect import create_spark_session, get_logger
 from utils.config import (
     MINIO_TRAFFIC_PROCESSED_PATH,
     MINIO_WEATHER_PROCESSED_PATH,
@@ -30,7 +28,6 @@ def main():
         traffic_count = df.count()
         logger.info(f"---------- PROCESSED TRAFFIC DATA FROM MINIO SUCESSFULLY READED --------- ")
         
-        # save_in_db(df, DB_TRAFFIC_TABLE)
         save_only_new_rows(spark, df, DB_TRAFFIC_TABLE)
         logger.info(f"--------------The {traffic_count} TRAFFIC Data Saving Sucessfully to Table {DB_TRAFFIC_TABLE}")
     
@@ -43,17 +40,18 @@ def main():
     # -------------------  Read WEATHER DATA from MinIO --------------------------------
 
     
-    # try:
+    try:
         
-    #     df = read_parquet_from_minio(spark, f"s3a://{SMART_CITY_BUCKET}/{MINIO_WEATHER_PROCESSED_PATH}")
-    #     weather_count = df.count()  
-    #     save_in_db(df, DB_WEATHER_TABLE)
-    #     logger.info(f"-------------- THE {weather_count} WEATHER Data Saving Sucessfully to Table {DB_WEATHER_TABLE}")
+        df = read_parquet_from_minio(spark, f"s3a://{SMART_CITY_BUCKET}/{MINIO_WEATHER_PROCESSED_PATH}")
+        weather_count = df.count()  
+
+        save_only_new_rows(spark, df, DB_WEATHER_TABLE)
+        logger.info(f"-------------- THE {weather_count} WEATHER Data Saving Sucessfully to Table {DB_WEATHER_TABLE}")
     
-    # except AnalysisException as e:
-    #     logger.error(f"❌ ------ FAILED TO LOAD Processed TRAFFIC DATA To DB Table {DB_WEATHER_TABLE} " + str(e))
-    #     spark.stop()
-    #     return
+    except AnalysisException as e:
+        logger.error(f"❌ ------ FAILED TO LOAD Processed WEATHER DATA To DB Table {DB_WEATHER_TABLE} " + str(e))
+        spark.stop()
+        return
    
 
     spark.stop()
