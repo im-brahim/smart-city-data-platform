53a8676810ef2f0b175067cc7f1300901fafd47c
ibrahim <ibrahimelaidouni@gmail.com>
2026-08-02 12:47:51 +0100

    feat: add track-pipline table that store the latest timestamp processed and from bronze to silver

--- init-db.sql
+++ init-db.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE airflow OWNER ibrahim;
 
 CREATE DATABASE smartcity OWNER ibrahim;
+
+
+\c smartcity
+
+CREATE TABLE pipeline_track (
+    "source" varchar(10) primary key, 
+    "last_processed" timestamp 
+)
