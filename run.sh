8d36d278de66f5b84ead67973535d3b5499e52b1
ibrahim <ibrahimelaidouni@gmail.com>
2026-08-02 12:53:04 +0100

    feat: used ./run.sh job.py jar unsted of db_jat for jobs needing postgres jar

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Usage:
 # ./run.sh your_job.py                        (basic run)
-# ./run.sh your_job.py db_jar             (if job needs jars)
+# ./run.sh your_job.py jar             (if job needs jars)
 
 JOB=$1
 USE_JARS=$2
 
 JARS_OPTION=""
-if [ "$USE_JARS" == "db_jar" ]; then
-  JARS_OPTION="--jars /opt/spark/jars/postgresql-42.6.0.jar"
+if [ "$USE_JARS" == "jar" ]; then
+  JARS_OPTION="--jars /opt/bitnami/spark/jars/postgresql-42.6.0.jar"
 fi
 
 docker exec -it master bash -c "export PYTHONPATH=\$PYTHONPATH:/opt/spark && spark-submit $JARS_OPTION /opt/spark/jobs/$JOB"
